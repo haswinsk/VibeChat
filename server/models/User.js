0e09edd5f05707471d5b31f97ceaccf2c03b516f
haswinsk <skhaswin@gmail.com>
2026-05-19 11:27:17 +0530

    Critical fix: remove next() calls from async pre-save hook

--- server/models/User.js
+++ server/models/User.js
@@ -49,16 +49,14 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
-    next();
     return;
   }
 
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
   } catch (error) {
-    next(error);
+    throw error;
   }
 });
 
